5b986b42ff3daedafa49ceeb2742a709e1acf926
Erick Rodríguez <e12antonio@gmail.com>
2022-08-15 23:04:16 -0600

    services added

--- src/components/CardHabilities.jsx
+++ src/components/CardHabilities.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { motion } from 'framer-motion'
 
-const CardHabilities = ({hability, description, image, animate=null, initial=null, transition=null}) => {
+const CardHabilities = ({hability, description, image, animate=null, initial=null, transition=null, widthImg, heigthImg}) => {
   return (
     <motion.div 
        className='p-4 border border-emerald-500 flex flex-col gap-1 items-center w-[300px] rounded-xl transition-transform transform hover:scale-110 bg-stone-900 text-slate-300'
@@ -12,7 +12,7 @@ const CardHabilities = ({hability, description, image, animate=null, initial=nul
       initial={initial}
       transition={transition} 
       >
-        <Image src={image} width='120px' height='120px' />
+        <Image src={image} width={widthImg} height={heigthImg} />
       </motion.div>
       <h4 className="text-lg font-semibold">{ hability }</h4>
       <p className="mt-2">{ description }</p>
---
+++ src/containers/Services.jsx
@@ -0,0 +1,52 @@
+import CardHabilities from '@components/CardHabilities';
+
+//import images
+import reactIcon from '@images/react-icon.png';
+import frontendIcon from '@images/frontend.svg';
+import servidorBases from '@images/servidor-bases-de-datos.png';
+
+const Services = () => {
+  return (
+    <div className="flex flex-col items-center  gap-5">
+      <div className="p-2 pt-8 w-max flex flex-col items-center border-b border-emerald-500">
+        <h3 className="font-semibold text-lg">Servicios</h3>
+        <span>¿Qué puedo hacer?</span>
+      </div>
+      <div className="flex justify-center flex-wrap p-4 font-Roboto gap-4 mx-[50px]">
+        <CardHabilities
+          image={reactIcon}
+          hability="React JS Frontend"
+          description="Construir interfaces de usuario para aplicaciones web escalables con React.js. Integrar React a una aplicación existente para mejorarla."
+          widthImg='120px'
+          heigthImg='120px'
+          animate={{ rotate: 360 }}
+          initial={{ rotate: 0 }}
+          transition={{ repeat: Infinity, duration: 5, delay: 0.2, ease: 'linear' }}
+        />
+        <CardHabilities
+          image={frontendIcon}
+          hability="Vanilla JS Frontend"
+          description="Páginas web de mediana y baja complejidad como Blogs, Portales Web y Sitios Estáticos. Consultas Integradas a APIs propias o de terceros."
+          widthImg='190px'
+          heigthImg='120px'
+          animate={{ scale: [1.15, 1.05, 1.15] }}
+          initial={{ scale: 1.15 }}
+          transition={{ repeat: Infinity, duration: 2.4, delay: 0.2, ease: 'linear' }}
+        />
+
+        <CardHabilities
+          image={servidorBases}
+          hability="Servidor y Bases de Datos Backend"
+          description="Modelo de Bases de datos para almacenar información intercambiable dinámicamente."
+          widthImg='190px'
+          heigthImg='120px'
+          animate={{ scale: [1.05, 1.15, 1.05] }}
+          initial={{ scale: 1.05 }}
+          transition={{ repeat: Infinity, duration: 2.4, delay: 0.2, ease: 'linear' }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Services

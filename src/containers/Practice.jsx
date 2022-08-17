66d4409c1a2d2261a69924aea13db08ce0cbba59
Erick Rodríguez <e12antonio@gmail.com>
2022-08-17 13:45:34 -0600

    bug of the screen fixed

--- src/containers/Practice.jsx
+++ src/containers/Practice.jsx
@@ -1,12 +1,12 @@
 import Project from '@components/Project'
-import rmImg from '@images/R&M-Project.jpg';
+import clockImg from '@images/clock-practice.jpg';
 
 const Practice = () => {
   return (
     <div className="flex flex-col items-center gap-5 mt-20">
-      <div className="p-2 pt-8 w-max flex flex-col items-center border-b border-emerald-500">
+      <div className="p-2 pt-8 flex flex-col items-center border-b border-emerald-500">
         <h3 className="font-semibold text-xl">Practicas</h3>
-        <span>Practicas realizadas a lo largo del aprendizaje de las mismas.</span>
+        <span className='text-center'>Practicas realizadas a lo largo del aprendizaje de las mismas.</span>
       </div>
       <div className='flex flex-wrap lg:gap-x-32 lg:gap-y-12 gap-4 justify-center '>
       <Project 
@@ -14,7 +14,7 @@ const Practice = () => {
           description='Creando un Reloj con Javascript y css.'
           deploySite='/daltocourse/clock'
           githubCode='https://github.com/ErickOwo/Curso-Javascript-Dalto/tree/main/Capitulo%2015'
-          image={rmImg}
+          image={clockImg}
           practice={true} />
       </div>
     </div>
